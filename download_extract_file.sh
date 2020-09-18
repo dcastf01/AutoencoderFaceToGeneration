1355b0e07430bdd34e21405545951c457f85f3b3
dcastf01 <59411917+dcastf01@users.noreply.github.com>
2020-09-18 12:27:00 +0200

    Update download_extract_file.sh

--- download_extract_file.sh
+++ download_extract_file.sh
@@ -13,19 +13,19 @@ filename="part1.tar.gz"
 downloading $fileid $filename
 mv $filename $folderTarFile
 tar -xf $folderTarFile"/"$filename -C $folderDatasetFile --strip-components 1
-rm $filename
+rm $folderTarFile"/"$filename
 
 fileid="0BxYys69jI14kNEt1SnNJN1Z2WWc"
 filename="part2.tar.gz"
 downloading $fileid $filename
 mv $filename $folderTarFile
 tar -xf $folderTarFile"/"$filename -C $folderDatasetFile --strip-components 1
-rm $filename
+rm $folderTarFile"/"$filename
 
 fileid="0BxYys69jI14kVkVTZHZHa21zUXM"
 filename="part3.tar.gz"
 downloading $fileid $filename
 mv $filename $folderTarFile
 tar -xf $folderTarFile"/"$filename -C $folderDatasetFile --strip-components 1
-rm $filename
+rm $folderTarFile"/"$filename
 
